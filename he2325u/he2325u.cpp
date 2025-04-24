310f91b4d72eaba113b982ad609d69ad5d00ce26
stefan <s.wosnik@ing-buero-wosnik.de>
2025-04-24 16:36:29 +0200

    build under windows 10 mingw64

--- he2325u/he2325u.cpp
+++ he2325u/he2325u.cpp
@@ -46,6 +46,21 @@
 	#include <unistd.h>
 #endif
 
+#ifdef _WIN32
+void usleep(__int64 usec)
+{
+    HANDLE timer;
+    LARGE_INTEGER ft;
+
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+    timer = CreateWaitableTimer(NULL, TRUE, NULL);
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+    WaitForSingleObject(timer, INFINITE);
+    CloseHandle(timer);
+}
+#endif
+
 int main(int argc, char* argv[])
 {
 	int res;
